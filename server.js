664ba9f570871e8a066fe4cb18bf006cf8388565
santi101093 <s_garcete@hotmail.com>
2016-12-01 15:08:55 -0300

    Se agregó la funcionalidad del punto dos en el archivo server.js

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
-var http = require("http");
-var server= http.createServer();
+var http=require('http');
+var url=require('url');
+var fs=require('fs');
 
-server.on("request", function(req,res){
-	res.end(getHour());
+var urlData = url.parse(req.url,true);
+var pathname = "public" + urlData.pathname;
+fs.readFile('pathname', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(process.env.PORT || 3000);
 });
 
-server.listen(process.env.PORT || 3000);
+// var http = require("http"); //ej anterior de reloj
+// var server= http.createServer();
 
-function getHour(){
-	var currentdate = new Date();
-	var datetime = "" + currentdate.getHours() + ":" + currentdate.getMinutes() + ":" + currentdate.getSeconds();
-	return datetime;
-}
+// server.on("request", function(req,res){
+// 	res.end(getHour());
+// });
+
+// server.listen(process.env.PORT || 3000);
+
+// function getHour(){
+// 	var currentdate = new Date();
+// 	var datetime = "" + currentdate.getHours() + ":" + currentdate.getMinutes() + ":" + currentdate.getSeconds();
+// 	return datetime;
+// }
