c167a9936f167a864a0240c24836dafe79a66e8d
santi101093 <s_garcete@hotmail.com>
2016-12-01 15:38:22 -0300

    Ejercicio funcionando. Falta verificar si existe

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 var http=require('http');
 var url=require('url');
 var fs=require('fs');
+var server= http.createServer();
 
-var urlData = url.parse(req.url,true);
-var pathname = "public" + urlData.pathname;
-fs.readFile('pathname', function (err, html) {
-    if (err) {
-        throw err; 
-    }       
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(process.env.PORT || 3000);
+server.on("request", function(req,res){
+	var urlData = url.parse(req.url,true);
+	var pathname = "public" + urlData.pathname;
+	
+	fs.readFile(pathname, function (err, html) {
+		    if (err) {
+		        throw err; 
+		    }       
+		    res.end(html);
+	});
 });
+
+server.listen(process.env.PORT || 3000);
+
 
 // var http = require("http"); //ej anterior de reloj
 // var server= http.createServer();
